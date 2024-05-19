b0ae1ccb9b5aabb3ddb7a7cae7aae776ec515427
Md. Sayeed Hassan Maheen <sayeedhassan1124@gmail.com>
2024-05-19 16:22:05 +0600

    19.05.24

--- lib/models/transaction_model.dart
+++ lib/models/transaction_model.dart
@@ -1,19 +1,40 @@
 class Transaction {
   final int id;
+  final String transactionNo;
+  final int type;
   final double amount;
-  final String date;
+  final String transactionDate;
+  final String details;
+  final String billNo;
+  final String? image;
+  final String? imagePath;
+  final int status;
 
   Transaction({
     required this.id,
+    required this.transactionNo,
+    required this.type,
     required this.amount,
-    required this.date,
+    required this.transactionDate,
+    required this.details,
+    required this.billNo,
+    this.image,
+    this.imagePath,
+    required this.status,
   });
 
   factory Transaction.fromJson(Map<String, dynamic> json) {
     return Transaction(
       id: json['id'],
+      transactionNo: json['transaction_no'],
+      type: json['type'],
       amount: double.parse(json['amount']),
-      date: json['transaction_date'],
+      transactionDate: json['transaction_date'],
+      details: json['details'],
+      billNo: json['bill_no'],
+      image: json['image'],
+      imagePath: json['image_full_path'],
+      status: json['status'],
     );
   }
 }
--- lib/screens/transection_screen.dart
+++ lib/screens/transection_screen.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:skill_test_app/utils/app_colors.dart';
 
 import '../controllers/transection_controller.dart';
+import '../utils/strings.dart';
+import '../widgets/custom_appbar.dart';
 
 class TransactionListScreen extends StatelessWidget {
   final TransactionController transactionController =
       Get.put(TransactionController());
 
+  String capitalize(String s) => s[0].toUpperCase() + s.substring(1);
   TransactionListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Transaction List'),
-      ),
+      backgroundColor: colorWhite,
+      appBar: const CustomAppBar(text: transactionsScreen),
       body: Obx(
         () => transactionController.isLoading.value
             ? const Center(
@@ -31,15 +35,71 @@ class TransactionListScreen extends StatelessWidget {
                   )
                 : ListView.builder(
                     itemCount: transactionController.transactions.length,
+                    padding: REdgeInsets.only(left: 16, right: 16, top: 16),
                     itemBuilder: (context, index) {
                       final transaction =
                           transactionController.transactions[index];
-                      return ListTile(
-                        title: Text(transaction.date),
-                        subtitle: Text('Amount: ${transaction.amount}'),
+                      return Container(
+                        width: double.infinity,
+                        padding: REdgeInsets.all(16),
+                        margin: REdgeInsets.only(bottom: 12),
+                        decoration: BoxDecoration(
+                          color: colorGreenLight,
+                          borderRadius: BorderRadius.circular(8.r),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Transaction No: ${transaction.transactionNo}',
+                              style: TextStyle(
+                                fontSize: 16.sp,
+                                fontWeight: FontWeight.w600,
+                                color: colorBlack,
+                              ),
+                            ),
+                            Gap(8.h),
+                            Divider(
+                              color: colorGreen,
+                              height: 1.h,
+                            ),
+                            Gap(8.h),
+                            DetailsTextWidget('Type: ${transaction.type}'),
+                            Gap(2.h),
+                            DetailsTextWidget('Amount: ${transaction.amount}'),
+                            Gap(2.h),
+                            DetailsTextWidget(
+                                'Transaction Date: ${transaction.transactionDate}'),
+                            Gap(2.h),
+                            DetailsTextWidget(
+                                'Details: ${capitalize(transaction.details)}'),
+                            Gap(2.h),
+                            DetailsTextWidget('Bill No: ${transaction.billNo}'),
+                            Gap(2.h),
+                            DetailsTextWidget('Status: ${transaction.status}'),
+                          ],
+                        ),
                       );
                     },
                   ),
+      ),
+    );
+  }
+}
+
+class DetailsTextWidget extends StatelessWidget {
+  final String text;
+
+  const DetailsTextWidget(this.text, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 14.sp,
+        fontWeight: FontWeight.w500,
+        color: colorBlack,
       ),
     );
   }
